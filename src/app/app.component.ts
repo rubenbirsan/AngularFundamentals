177c82c79df2f1a029907730ae610628f6abab69
Ruben Birsan <ruben.birsan1@gmail.com>
2022-10-23 20:24:26 +0200

    feat: install angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,27 +4,43 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
-    <div>
-      <h1>
-        {{ title + '!' }}
-      </h1>
-      <h1 [innerHtml]="title"></h1>
-      <img [src]="logo" >
-      <input type="text" [value]="name">
-      <div>{{name}}</div>
-    </div>
+    <section>
+      <div class="example-button-row">
+        <button mat-raised-button color="primary" (click)="handleClick($event)">Change Name</button>
+      </div>
+      <input matInput placeholder="Ex. Pizza"
+        type="text"
+        [value]="name"
+        (blur)="handleBlur($event)"
+        (input)="handleInput($event)"
+      />
+      <div>{{ name }}</div>
+    </section>
   `,
 })
 export class AppComponent {
   title: string;
-
   number1: number = 1;
   number2: number = 2;
-
-  logo: string = 'assets/img/angular.svg'
+  logo: string = 'assets/img/angular.svg';
   isHappy: boolean = false;
   name: string = 'Todd';
+
   constructor() {
     this.title = 'Angular Ultimate';
+  }
+
+  handleBlur(event: any) {
+    console.log(event);
+    this.name = event.target.value;
+  }
+
+  handleInput(event: any) {
+    console.log(event);
+    this.name = event.target.value;
+  }
+  handleClick(event: any) {
+    console.log(event);
+    this.name = "Motto";
   }
 }

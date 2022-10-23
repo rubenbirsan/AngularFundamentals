972d2fb4a01b591d7ad86b439c989a88363ea0d0
Ruben Birsan <ruben.birsan1@gmail.com>
2022-10-23 20:00:46 +0200

    feat: test one way binding for inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,30 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
-    <div class="app">
-      {{title}}
+    <div>
+      <h1>
+        {{ title + '!' }}
+      </h1>
+      <h1 [innerHtml]="title"></h1>
+      <img [src]="logo" >
+      <input type="text" [value]="name">
+      <div>{{name}}</div>
     </div>
-  `
+  `,
 })
-
-export class AppComponent{
+export class AppComponent {
   title: string;
-  constructor(){
+
+  number1: number = 1;
+  number2: number = 2;
+
+  logo: string = 'assets/img/angular.svg'
+  isHappy: boolean = false;
+  name: string = 'Todd';
+  constructor() {
     this.title = 'Angular Ultimate';
   }
 }
